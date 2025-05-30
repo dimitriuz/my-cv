38c5da1f82361ae81f60324f7b75f0b270d2bd7d
Dmitrii Leshchenko <dimitriuz@gmail.com>
2025-05-30 11:48:35 +0200

    feat: update Vite configuration to dynamically set base path based on command

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
 import { resolve, dirname } from "node:path";
 import VueI18nPlugin from "@intlify/unplugin-vue-i18n/vite";
+
 // https://vite.dev/config/
-export default defineConfig({
-  base: '/my-cv/',
+export default defineConfig(({ command }) => ({
+  base: command === 'serve' ? '/' : '/my-cv/',
   plugins: [vue(),
     VueI18nPlugin({
       include: resolve(dirname(fileURLToPath(import.meta.url)), './src/locales/**'),
@@ -29,5 +30,10 @@ export default defineConfig({
         entryFileNames: 'assets/[name]-[hash].js'
       }
     }
+  },
+  preview: {
+    port: 4173,
+    strictPort: true,
+    base: '/my-cv/'
   }
-})
+}))
